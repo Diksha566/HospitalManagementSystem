99e5f7b94c5a152b892789c83bbc2b308cc669f4
Diksha566 <dikshajethwa04@gmail.com>
2025-09-26 18:55:03 +0530

    Initial commit

---
+++ HospitalManagementSystem/Patients.java
@@ -0,0 +1,10 @@
+package HospitalManagementSystem;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Patients{
+    private Connection Connection;
+    private Scanner scanner;
+
+    
+}
